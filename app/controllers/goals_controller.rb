b65169f13d0484b2ebd832a58f0117e919e0d79b
Jessica Wong <jesswon777@yahoo.com.au>
2022-01-04 21:44:01 +1100

    added create and new to goals controller and validations for goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,33 @@
 class GoalsController < ApplicationController
+  def index
+    @goals = Goal.all
+  end
+
+  def show
+    find_goal
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user = current_user
+    if @goal.save
+      redirect_to goals_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
+  def goal_params
+    params.require(:goal).permit(:title, :description, :amount, :date, :user_id)
+  end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,9 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  validate :goal_date_before_today
+  validates :title, :description, :date, :amount, presence: true
+
+  def goal_date_before_today
+    errors.add(:start_date, "must be after today") if date <= Date.today
+  end
 end

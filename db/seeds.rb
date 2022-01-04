d0208b39207d272236cd624599255ee39987efa5
David Deegan <daviddeegan@gmail.com>
2022-01-04 18:04:55 +0800

    V1 of the seed file complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+
+User.destroy_all
+Goal.destroy_all
+
+puts "Creating Users..."
+
+4.times do
+  user = User.create(
+    first_name: "#{Faker::Name.first_name}",
+    last_name: "#{Faker::Name.last_name}",
+    email: "#{Faker::Internet.email}",
+    password: "dollar123",
+    investment: rand(500..1000)
+  )
+
+  puts "Created User #{user.first_name} #{user.last_name}"
+end
+
+user = User.create(
+  first_name: "David",
+  last_name: "AJ Wong",
+  email: "somethingwong@gmail.com",
+  password: "123456",
+  investment: rand(500..1000)
+)
+
+puts "Creating Goals..."
+
+user = 0
+5.times do
+  user += 1
+  3.times do
+    goal = Goal.create(
+      title: "#{Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 3)}",
+      description: "#{Faker::Lorem.sentence(word_count: 8, supplemental: true, random_words_to_add: 7)}",
+      amount: rand(200..500),
+      date: Date.strptime("#{Faker::Date.between(from: '2022-06-01', to: '2027-06-01')}", "%Y-%m-%d"),
+      user_id: user
+    )
+    puts "Created Goal #{goal.title} for user ID #{user}"
+  end
+end
+
+puts "Finished!"

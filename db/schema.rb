3e5553a4de5f58daf924a111dceb9ff1ec2302a7
Dylan Pee <peedylan94@gmail.com>
2022-01-08 14:29:10 +1030

    changes to schema removed space

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_01_06_083108) do
+ActiveRecord::Schema.define(version: 2022_01_06_083750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +23,8 @@ ActiveRecord::Schema.define(version: 2022_01_06_083108) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "recurring_investment"
     t.boolean "archived"
+    t.float "recurring_investment"
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 

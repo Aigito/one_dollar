d52141a1b89cb0be42dd02a6619b3ee735e3d31c
AJ Yee <ayee@thoughtworks.com>
2022-01-12 16:07:17 +1100

    Added ability for users to add profile picture from their edit users page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+protected
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name investment])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name investment])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name investment avatar])
   end
 end

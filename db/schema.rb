14baa42218ffb886b86bd4e457ec14e8c6303a71
Dylan Pee <peedylan94@gmail.com>
2022-01-14 15:16:56 +1030

    Various fixups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_093244) do
+ActiveRecord::Schema.define(version: 2022_01_08_053718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2022_01_11_093244) do
     t.string "last_name"
     t.integer "investment"
     t.integer "invested", default: 0
-    t.boolean "new_user", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

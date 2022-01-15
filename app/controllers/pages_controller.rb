47ccb49304d8cccb34d8e1df1cc6bcfcf629648d
Dylan Pee <peedylan94@gmail.com>
2022-01-15 13:27:56 +1030

    Moved tax calculator to another page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,7 @@ class PagesController < ApplicationController
     @investment = @user.investment
     @percentage_completion = @invested.to_f / @investment * 100
   end
+
+  def taxcalc
+  end
 end

a1d77d58fde4bf353b34132455274941f6408ece
Dylan Pee <peedylan94@gmail.com>
2022-01-27 21:12:06 +1030

    Changed suggestions to only 2 suggestions

--- app/javascript/plugins/goalsuggestor.js
+++ app/javascript/plugins/goalsuggestor.js
@@ -1,17 +1,7 @@
 const goalsuggestor = (() => {
   const suggestions = [
-      "Give to charity",
-      "Build up emergency fund",
-      "Backpacking trip",
-      "New pair of running shoes",
-      "Hire a clown",
-      "Nice bottle of wine",
-      "Gift to a friend",
-      "Retirement!!!",
       "Down payment for a horse, yes...a horse",
-      "Le Wagon Coding Bootcamp",
-      "Index Fund / EFTs",
-      "Gym Equipment"
+      "Le Wagon Coding Bootcamp"
     ]
 
   let currentSuggestion = ""
